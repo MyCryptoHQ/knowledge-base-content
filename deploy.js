52e158057fca62fae8ffc205a93994d69f1a65c8
Mrtenz <maarten@zuidhoorn.com>
2018-12-20 22:47:32 +0100

    Fix Travis and add token

--- deploy.js
+++ deploy.js
@@ -3,7 +3,7 @@ const request = require('request');
 /**
  * Send a request to Travis to rebuild the main knowledge base repo with the updated content.
  */
-request.post('https://api.travis-ci.com/repo/MyCryptoHQ%2Fknowledge-base/requests', {
+request.post('https://api.travis-ci.org/repo/MyCryptoHQ%2Fknowledge-base/requests', {
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
